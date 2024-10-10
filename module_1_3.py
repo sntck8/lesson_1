18f84730d4a8af8a41ce225567c20039e6a5914f
sntck8 <sntck88@gmail.com>
2024-10-11 06:39:58 +0700

    module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -3,9 +3,9 @@ print("Name: " + name)
 age: int =  35
 age = str(35)
 print("Age: " + age)
-new_age: int = 45
-new_age = str(45)
-print("New Age: " + new_age)
+age = 35
+new_age = (age + 1)
+print("New Age: " + str(new_age))
 is_student: True
 is_student = str(True)
 print("Is student: " + is_student)
