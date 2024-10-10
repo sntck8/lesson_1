b370359177a4f435ed796e049f58fa06613137e4
sntck8 <sntck88@gmail.com>
2024-10-10 21:14:03 +0700

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,11 @@
+name: str = "Aleksandr"
+print("Name: " + name)
+age: int =  35
+age = str(35)
+print("Age: " + age)
+new_age: int = 45
+new_age = str(45)
+print("New Age: " + new_age)
+is_student: True
+is_student = str(True)
+print("Is student: " + is_student)
