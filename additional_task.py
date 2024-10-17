5788105da2788478dff2487737f1ac900d81402c
sntck8 <sntck88@gmail.com>
2024-10-17 20:04:11 +0700

    Update additional_task.py

--- additional_task.py
+++ additional_task.py
@@ -1,2 +1,12 @@
 grades = [[5,3,3,5,4],[2,2,2,3],[4,5,5,2],[4,4,3],[5,5,5,4,5]]
-students = {"Johnny", "Bilbo", "Steve", "Khendrik", "Aaron"}
+students = {"Johnny", "Bilbo", "Steve", "Kendrick", "Aaron"}
+dictionary = {}
+names = list(students)
+names.sort()
+# print(names)
+dictionary.update({names[0]: sum(grades[0])/len(grades[0]),
+                   names[1]: sum(grades[1])/len(grades[1]),
+                   names[2]: sum(grades[2])/len(grades[2]),
+                   names[3]: sum(grades[3])/len(grades[3]),
+                   names[4]: sum(grades[4])/len(grades[4]),})
+print("Средний балл учеников: ", dictionary)
